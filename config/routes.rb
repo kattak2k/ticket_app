d6bdb6d78bcbe272473398d3545a5dbf4e7865f6
Jordan Owens <jordan.owens@nasa.gov>
2014-07-15 17:58:03 -0400

    Update to shallow routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    resources :projects, only: [:index, :create, :update, :destroy] do
-      resources :tickets, only: [:index, :create, :update, :destroy]
+    resources :projects, except: [:show, :new], shallow: true do
+      resources :tickets, except: [:show, :new], shallow: true do
+        resources :comments, except: [:index, :show, :new]
+      end
     end
   end
 end
--- test/integration/delete_ticket_test.rb
+++ test/integration/delete_ticket_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class DeleteTicketTest < ActionDispatch::IntegrationTest
   setup do
-    @project = Project.create!(name: 'Test Project')
-    @ticket = @project.tickets.create(subject: 'Test Issue', description: 'This is a test.')
+    project = Project.create!(name: 'Test Project')
+    @ticket = project.tickets.create(subject: 'Test Issue', description: 'This is a test.')
   end
 
   test 'delete project' do
-    delete "/api/projects/#{@project.id}/tickets/#{@ticket.id}"
+    delete "/api/tickets/#{@ticket.id}"
     assert_equal 204, response.status
   end
 end
