f7dbe908efd5dcf6958070a14cc9c879c37b0936
Jordan Owens <jordan.owens@nasa.gov>
2014-08-01 13:55:32 -0400

    Update migration for pg

--- db/migrate/20140716015605_change_status_and_priority_to_integer.rb
+++ db/migrate/20140716015605_change_status_and_priority_to_integer.rb
@@ -1,5 +1,7 @@
 class ChangeStatusAndPriorityToInteger < ActiveRecord::Migration
   def change
+    change_column_default :tickets, :status, nil
+    change_column_default :tickets, :priority, nil
     change_column :tickets, :status, 'integer USING (trim(status)::integer)', default: 0
     change_column :tickets, :priority, 'integer USING (trim(priority)::integer)', default: 0
   end
